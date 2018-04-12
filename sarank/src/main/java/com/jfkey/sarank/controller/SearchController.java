63272b0254264d422325fb0bc7af81476d82a5d3
jf501 <742718933@qq.com>
2018-04-12 23:29:43 +0800

    v0.1.1 search page changes

---
+++ sarank/src/main/java/com/jfkey/sarank/controller/SearchController.java
@@ -0,0 +1,30 @@
+package com.jfkey.sarank.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.jfkey.sarank.domain.SearchPara;
+import com.jfkey.sarank.service.PaperInfoService;
+
+/**
+ * 
+ * @author junfeng Liu
+ * @time 10:35:30 PM Apr 12, 2018
+ * @version v0.1.1
+ * @desc search controller
+ */
+@Controller
+public class SearchController {
+	@Autowired
+	private PaperInfoService paperInfoService;
+	
+	@RequestMapping(value="/search", method=RequestMethod.GET)
+	public String search(@ModelAttribute(value = "para") SearchPara searchPara) {
+		
+		
+		return null;
+	}
+}
